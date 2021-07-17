f12a036bbf67ca8930a2f7795f28bb216d34c44f
Ricardo Costa <ricardo.costa@checkatrade.com>
2021-07-17 18:48:40 +0100

    quick change

--- apiService/apiService/Startup.cs
+++ apiService/apiService/Startup.cs
@@ -25,7 +25,6 @@ namespace apiService
         // This method gets called by the runtime. Use this method to add services to the container.
         public void ConfigureServices(IServiceCollection services)
         {
-
             services.AddControllers();
         }
 
